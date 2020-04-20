75e6f28a884c8b1deedd4f836a04385fe9ad8d92
ignacio <ignaciomenchaca666@gmail.com>
2020-04-19 23:01:32 -0500

    Initial commit

---
+++ Delegates/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        public delegate float DelegateOperation(float val1 , float val2);
+        
+        public static float DepositOfMoney(float quantity, float amount)
+        {
+            return quantity + amount;
+        }
+
+        public static float WithdrawalOfMoney(float quantity, float amount)
+        {
+            if (!(quantity > amount)) 
+                return amount - quantity;
+            Console.WriteLine("insufficient funds");
+            return 0.0f;
+        }
+
+        public static float ExecuteOperation(DelegateOperation operation, float quantity, float amount)
+        {
+            return operation(quantity, amount);
+        }
+        
+        public static void Main(string[] args)
+        {
+            DelegateOperation retirement = WithdrawalOfMoney;
+            DelegateOperation deposit = DepositOfMoney;
+            
+            var resultRetirement = ExecuteOperation(retirement, 100, 500);
+            var resultDeposit = ExecuteOperation(deposit, 100, 500);
+            
+            Console.WriteLine($"Retirement: {resultRetirement}");
+            Console.WriteLine($"Deposit: {resultDeposit}");
+
+            // Lambda + delegates
+            DelegateOperation depositInterest = (quantity, amount) => (quantity > 100) ? quantity + amount + (amount * 0.02f) : quantity + amount;
+            var resultLambda = ExecuteOperation(depositInterest, 101, 100 );
+            Console.WriteLine($"Interest: {resultLambda}");
+        }
+    }
+}
---
+++ Lambdas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lambdas
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* lambda = anonymous function */
+            
+            // one param lambda function
+            Func<int, bool> isEven = number => number % 2 == 0;
+            Console.WriteLine($"The number is even: {isEven(10)}");
+            
+            // two params lambda function
+            Func<int, int, int> sum = (num1, num2) => num1 + num2;
+            Console.WriteLine($"The sum is: {sum(3, 5)}");
+            
+            // Body lambda function
+            Func<string, bool> validEmail = s =>
+            {
+                if (s.EndsWith("@gmail.com", StringComparison.CurrentCulture)) 
+                    return true;
+                Console.WriteLine("> this email is invalid.");
+                return false;
+            };
+            Console.WriteLine($"The email is valid");
+            
+            // Lambda expressions functions without return
+            Action<string> print = s => Console.WriteLine(s);
+            print("I am lambda function!");
+        }
+    }
+}
